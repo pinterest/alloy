fede1e77b133ce9b9ee62f61cf5f698c28ce6e46
Mauricio Gardini <mgardini@pinterest.com>
2025-11-13 23:52:27 +0000

    Add Full Language support

---
+++ packages/graphql/src/components/FragmentSpread.tsx
@@ -0,0 +1,48 @@
+import { Children, Show } from "@alloy-js/core";
+
+export interface FragmentSpreadProps {
+  /**
+   * The name of the fragment to spread (or a refkey to a fragment)
+   */
+  name: Children;
+  /**
+   * Directives to apply to the fragment spread
+   */
+  directives?: Children;
+}
+
+/**
+ * A fragment spread in a GraphQL operation.
+ *
+ * @example
+ * ```tsx
+ * import { refkey } from "@alloy-js/core";
+ *
+ * const userFragmentRef = refkey();
+ *
+ * // Using fragment name directly
+ * <FragmentSpread name="UserFields" />
+ *
+ * // Using refkey
+ * <FragmentSpread name={userFragmentRef} />
+ *
+ * // With directives
+ * <FragmentSpread
+ *   name="UserFields"
+ *   directives={<Directive name="include" arguments={<Argument name="if" value={<Variable name="includeUser" />} />} />}
+ * />
+ * ```
+ * renders to
+ * ```graphql
+ * ...UserFields
+ * ...UserFields @include(if: $includeUser)
+ * ```
+ */
+export function FragmentSpread(props: FragmentSpreadProps) {
+  return (
+    <>
+      ...{props.name}
+      <Show when={Boolean(props.directives)}>{props.directives}</Show>
+    </>
+  );
+}
