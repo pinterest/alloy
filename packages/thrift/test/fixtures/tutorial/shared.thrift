1763c1cda79a00fd4717b1b2ba32c630d563e58a
Steve Rice <srice@pinterest.com>
2026-02-24 12:01:53 -0800

    restore original fixtures

--- packages/thrift/test/fixtures/tutorial/shared.thrift
+++ packages/thrift/test/fixtures/tutorial/shared.thrift
@@ -6,7 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -23,7 +25,6 @@
 namespace cl shared
 namespace cpp shared
 namespace d share // "shared" would collide with the eponymous D keyword.
-
 namespace dart shared
 namespace java shared
 namespace perl shared
@@ -31,11 +32,12 @@ namespace php shared
 namespace haxe shared
 namespace netstd shared
 
+
 struct SharedStruct {
-    1: required i32 key,
-    2: required string value,
+  1: i32 key
+  2: string value
 }
 
 service SharedService {
-    SharedStruct getStruct(1: i32 key),
+  SharedStruct getStruct(1: i32 key)
 }
--- packages/thrift/test/fixtures/tutorial/tutorial.thrift
+++ packages/thrift/test/fixtures/tutorial/tutorial.thrift
@@ -6,7 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,15 +19,18 @@
 
 # Thrift Tutorial
 # Mark Slee (mcslee@facebook.com)
+#
 # This file aims to teach you how to use Thrift, in a .thrift file. Neato. The
 # first thing to notice is that .thrift files support standard shell comments.
 # This lets you make your thrift file executable and include your Thrift build
 # step on the top line. And you can place comments like this anywhere you like.
+#
 # Before running this file, you will need to have installed the thrift compiler
 # into /usr/local/bin.
 
 /**
  * The first thing to know about are types. The available types in Thrift are:
+ *
  *  bool        Boolean, one byte
  *  i8 (byte)   Signed 8-bit integer
  *  i16         Signed 16-bit integer
@@ -37,6 +42,7 @@
  *  map<t1,t2>  Map from one type to another
  *  list<t1>    Ordered list of one type
  *  set<t1>     Set of unique elements of one type
+ *
  * Did you also notice that Thrift supports C style comments?
  */
 
@@ -46,6 +52,7 @@
  * Thrift files can reference other Thrift files to include common struct
  * and service definitions. These are found using the current path, or by
  * searching relative to any paths specified with the -I compiler flag.
+ *
  * Included objects are accessed using the name of the .thrift file as a
  * prefix. i.e. shared.SharedObject
  */
@@ -77,40 +84,41 @@ typedef i32 MyInteger
  * types and structs are specified using JSON notation.
  */
 const i32 INT32CONSTANT = 9853
-const map<string, string> MAPCONSTANT = { 'hello' : 'world' , 'goodnight' : 'moon' }
+const map<string,string> MAPCONSTANT = {'hello':'world', 'goodnight':'moon'}
 
 /**
  * You can define enums, which are just 32 bit integers. Values are optional
  * and start at 1 if not supplied, C style again.
  */
 enum Operation {
-    ADD = 1,
-    SUBTRACT = 2,
-    MULTIPLY = 3,
-    DIVIDE = 4
+  ADD = 1,
+  SUBTRACT = 2,
+  MULTIPLY = 3,
+  DIVIDE = 4
 }
 
 /**
  * Structs are the basic complex data structures. They are comprised of fields
  * which each have an integer identifier, a type, a symbolic name, and an
  * optional default value.
+ *
  * Fields can be declared "optional", which ensures they will not be included
  * in the serialized output if they aren't set.  Note that this requires some
  * manual management in some languages.
  */
 struct Work {
-    1: required i32 num1 = 0,
-    2: required i32 num2,
-    3: required Operation op,
-    4: optional string comment,
+  1: i32 num1 = 0,
+  2: i32 num2,
+  3: Operation op,
+  4: optional string comment,
 }
 
 /**
  * Structs can also be exceptions, if they are nasty.
  */
 exception InvalidOperation {
-    1: required i32 whatOp,
-    2: required string why,
+  1: i32 whatOp,
+  2: string why
 }
 
 /**
@@ -119,25 +127,26 @@ exception InvalidOperation {
  */
 service Calculator extends shared.SharedService {
 
-    /**
-     * A method definition looks like C code. It has a return type, arguments,
-     * and optionally a list of exceptions that it may throw. Note that argument
-     * lists and exception lists are specified using the exact same syntax as
-     * field lists in struct or exception definitions.
-     */
+  /**
+   * A method definition looks like C code. It has a return type, arguments,
+   * and optionally a list of exceptions that it may throw. Note that argument
+   * lists and exception lists are specified using the exact same syntax as
+   * field lists in struct or exception definitions.
+   */
 
-    void ping(),
-    i32 add(1: i32 num1,
-    2: i32 num2),
-    i32 calculate(1: i32 logid,
-    2: Work w) throws(1: InvalidOperation ouch),
+   void ping(),
 
-    /**
-     * This method has a oneway modifier. That means the client only makes
-     * a request and does not listen for any response at all. Oneway methods
-     * must be void.
-     */
-    oneway void zip(),
+   i32 add(1:i32 num1, 2:i32 num2),
+
+   i32 calculate(1:i32 logid, 2:Work w) throws (1:InvalidOperation ouch),
+
+   /**
+    * This method has a oneway modifier. That means the client only makes
+    * a request and does not listen for any response at all. Oneway methods
+    * must be void.
+    */
+   oneway void zip()
+
 }
 
 /**
